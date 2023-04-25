9202d7c2ac0478e7f3d0e804782856d68692a16e
Luc-creat <131754944+Luc-creat@users.noreply.github.com>
2023-04-25 10:47:46 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+let Drehzahl = 0
 basic.forever(function () {
-	
+    Drehzahl = pins.analogReadPin(AnalogPin.P1)
+    pins.analogWritePin(AnalogPin.P1, Drehzahl)
+    if (Drehzahl > 0 == Drehzahl < 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Drehzahl < 0 && Drehzahl < 400) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Drehzahl < 0 && Drehzahl < 800) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Drehzahl < 0 && Drehzahl < 1023) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
 })
